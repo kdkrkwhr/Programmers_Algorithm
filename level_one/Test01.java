4823302309ed44be91b28c8eaa27ddcb39754986
KimDongki <kdkdongki1997@gmail.com>
2020-01-06 20:37:38 +0900

    레벨1

---
+++ level_one/Test01.java
@@ -0,0 +1,32 @@
+package level_one;
+
+import java.util.Arrays;
+
+// 완주하지 못한 선수
+public class Test01 {
+
+	public static void main(String[] args) {
+		String result;
+
+		String[] participant = {"leo", "kiki", "eden"};
+		String[] completion = {"eden", "kiki"};
+
+		result = solution(participant, completion);
+	}
+
+	public static String solution(String[] participant, String[] completion) {
+		int i;
+
+		Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for(i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i])) {
+                return participant[i];
+
+            }
+        }
+
+        return participant[i];
+    }
+}
---
+++ level_one/Test02.java
@@ -0,0 +1,44 @@
+package level_one;
+
+// 두 정수 사이의 합
+public class Test02 {
+
+	public static void main(String[] args) {
+		long result;
+
+		int a = 3;
+		int b = 5;
+
+		result = solution(a, b);
+	}
+
+	public static long solution(int a, int b) {
+		long answer = 0;
+		boolean calFlag = false;
+
+		if (a < b) {
+			calFlag = true;
+
+		} else if (a > b) {
+			calFlag = false;
+
+		} else {
+			return a;
+
+		}
+
+		if (calFlag) {
+			for (int i = a; i <= b; i++) {
+				answer = answer + i;
+			}
+
+		} else if (!calFlag) {
+			for (int i = b; i <= a; i++) {
+				answer = answer + i;
+
+			}
+
+		}
+		return answer;
+	}
+}
